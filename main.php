ec065a354be16d5faa6e412b5e676929ff39cad4
Vladimír Kriška <vlado@keboola.com>
2017-02-10 11:49:46 +0100

    Outpu also some data

--- main.php
+++ main.php
@@ -12,8 +12,16 @@ $usageFileContent = <<<JSON
         "metric": "words",
         "value": 123
     }
-]
+]\n
 JSON;
 
-$fs->mkdir('/data/out');
+$csvFileContent = <<<CSV
+id,value
+1,10
+2,20\n
+CSV;
+
+$fs->mkdir('/data/out/tables');
+
 $fs->dumpFile('/data/out/usage.json', $usageFileContent);
+$fs->dumpFile('/data/out/tables/test.csv', $csvFileContent);
